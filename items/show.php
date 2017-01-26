6084a67a7166c9836b1aa20a44e93e0a9702ca0d
Eric C. Weig <eweig@email.uky.edu>
2017-01-26 18:28:20 -0500

    Create show.php

---
+++ items/show.php
@@ -0,0 +1,75 @@
+<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
+<div id="primary">
+    <div id="container">
+<table><tr>
+<td><div id="right"><?php echo files_for_item(); ?></div></td>
+<td style="text-align:top;"><div id="left"><h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
+<?php if(metadata('item','Collection Name')): ?>
+<h3><?php echo __('Collection'); ?>: <?php echo link_to_collection_for_item(); ?></h3>
+<?php endif; ?>
+</div></td>
+</tr>
+</table>
+
+
+  <div id="tabs-container">
+    <ul class="tabs-menu">
+        <li class="current"><a href="#tab-1">Description</a></li>
+        <li><a href="#tab-2">Interview</a></li>
+    </ul>
+    <div class="tab">
+        <div id="tab-1" class="tab-content">
+<!-- Items metadata -->
+        <?php echo all_element_texts('item'); ?>
+
+        </div>
+        <div id="tab-2" class="tab-content">
+    <?php if(metadata('item', array('Item Type Metadata', 'OHMS Object'))): ?>
+        <?php echo metadata('item', array('Item Type Metadata', 'OHMS Object')); ?>
+    <?php endif; ?>
+        
+        </div>
+   
+    </div>
+</div>
+
+  
+
+
+
+
+
+<!-- The following prints a list of all tags associated with the item -->
+    <?php if (metadata('item','has tags')): ?>
+    <div id="item-tags" class="element">
+        <h3><?php echo __('Tags'); ?></h3>
+        <div class="element-text"><?php echo tag_string('item'); ?></div>
+    </div>
+
+
+    <?php endif;?>
+
+
+<!-- The following prints a citation for this item. -->
+    <div id="item-citation" class="element">
+        <h3><?php echo __('Citation'); ?></h3>
+        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+    </div>
+       <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+
+
+    <ul class="item-pagination navigation">
+        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+    </ul>
+
+</div> 
+
+<!-- End of Primary. -->
+
+</div>
+
+
+
+
+ <?php echo foot(); ?>
